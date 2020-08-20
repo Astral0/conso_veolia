7526d6171c4f3885923c5f1a4d9a146491c7f299
Astral0 <astral0@protonmail.com>
2020-08-20 09:57:56 +0000

    Add timeout mechanism

--- get_veolia_idf_consommation.py
+++ get_veolia_idf_consommation.py
@@ -11,6 +11,8 @@ import logging
 import logging.config
 logging.config.dictConfig(config.LOGGING_CONFIG)
 
+from interruptingcow import timeout
+time_out=300   # seconds
 
 class VeoliaIdf:
     def __init__(self):
@@ -30,13 +32,13 @@ class VeoliaIdf:
         profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
 
         self.browser = webdriver.Firefox(firefox_profile=profile, options=options, executable_path=config.GECKODRIVER_PATH, service_log_path=os.path.join(config.BASE_DIR, 'logs',  'geckodriver.log'))
-        
+
 
     def _purge_tmp(self):
         csv_files = glob.glob('%s/*.csv'%config.DOWNLOAD_DIR_TMP)
         for f in csv_files:
             os.remove(f)
-        
+
     def take_screenshot(self, name):
         logging.debug("Taking screenshot : %s"%name)
         fpath = os.path.join(config.BASE_DIR, 'logs', name)
@@ -59,26 +61,28 @@ class VeoliaIdf:
             password_field.clear()
             password_field.send_keys(config.VEOLIA_PASSWORD)
             time.sleep(2)
-            
+
             login_button = browser.find_element_by_xpath("//button[contains(.,'VALIDER')]")
-            
+
             self.take_screenshot("1_login_form")
 
             login_button.click()
             time.sleep(5)
             self.take_screenshot("2_logedin_form")
-            
+
             logging.debug('browsing to %s'%url_conso)
             browser.get(url_conso)
             time.sleep(15)
             self.take_screenshot("3_conso")
-            
+
             # downloadFileButton = browser.find_element_by_class_name("btn-green.slds-button.slds-button_icon.slds-text-title_caps")
             # downloadFileButton = browser.find_elements_by_css_selector(".slds-button_icon.slds-text-title_caps")
             downloadFileButton = browser.find_element_by_xpath("//button[contains(., 'charger la')]")
             downloadFileButton.click()
         except Exception as e:
             logging.error("Got on exception", exc_info=True)
+            print("\n\n\n\n-----------------------------------\n\n\n\n" + str(e) + "\n\n\n\n-----------------------------------\n\n\n\n")
+
             self.take_screenshot("on_exception")
             self.clean()
 
@@ -92,22 +96,29 @@ class VeoliaIdf:
         if len(r) != 91:
             logging.warning("Unexpected number of lines in csv file")
         j = json.dumps(r)
-        jeedom_php_history_veolia_path = os.path.join(config.BASE_DIR, "jeedom_php_history_veolia.php")
-        stdout_ = os.system(
-            "php {jeedom_php_history_veolia_path} {jeedom_cmd_index} {jeedom_cmd_conso_24h} '{json_data}'".format(
-                jeedom_php_history_veolia_path=jeedom_php_history_veolia_path,
-                jeedom_cmd_index=config.JEEDOM_CMD_INDEX, 
-                jeedom_cmd_conso_24h=config.JEEDOM_CMD_CONSO_24H, 
-                json_data=j
-            )
-        )
-        logging.debug(stdout_)
+#        jeedom_php_history_veolia_path = os.path.join(config.BASE_DIR, "jeedom_php_history_veolia.php")
+#        stdout_ = os.system(
+#            "php {jeedom_php_history_veolia_path} {jeedom_cmd_index} {jeedom_cmd_conso_24h} '{json_data}'".format(
+#                jeedom_php_history_veolia_path=jeedom_php_history_veolia_path,
+#                jeedom_cmd_index=config.JEEDOM_CMD_INDEX,
+#                jeedom_cmd_conso_24h=config.JEEDOM_CMD_CONSO_24H,
+#                json_data=j
+#            )
+#        )
+#        logging.debug(stdout_)
 
     def clean(self):
-        logging.debug("cleaning up")
-        self.browser.close()
-        self.display.stop()
-        
+        try:
+            with timeout(time_out, exception=RuntimeError):
+                logging.debug("cleaning up")
+                self.browser.close()
+                self.display.stop()
+                return
+        except RuntimeError:
+            print("Stopping receive after %s seconds" % time_out)
+            return
+
+
 if __name__ == '__main__':
     v = VeoliaIdf()
     try:
